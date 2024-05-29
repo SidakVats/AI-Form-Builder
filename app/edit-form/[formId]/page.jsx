fc891e643ad9102e4f6e84d80601b14972a7d622
SidakVats <sidakvats6499@gmail.com>
2024-05-29 18:06:44 +0530

    fetched data and showed data on ui

--- app/edit-form/[formId]/page.jsx
+++ app/edit-form/[formId]/page.jsx
@@ -1,11 +1,52 @@
-import React from 'react'
+"use client";
+import { db } from "@/configs";
+import { JsonForms } from "@/configs/schema";
+import { useUser } from "@clerk/nextjs";
+import { and, eq } from "drizzle-orm";
+import { ArrowLeft } from "lucide-react";
+import { useRouter } from "next/navigation";
+import React, { useEffect, useState } from "react";
+import FormUi from "../_components/FormUi";
 
-const EditForm = () => {
+function EditForm({ params }) {
+  const { user } = useUser();
+  const [jsonForm, setJsonForm] = useState([]);
+  const router = useRouter();
+
+  useEffect(() => {
+    user && GetFormData();
+  }, [user]);
+  const GetFormData = async () => {
+    const result = await db
+      .select()
+      .from(JsonForms)
+      .where(
+        and(
+          eq(JsonForms.id, params?.formId),
+          eq(JsonForms.createdBy, user?.primaryEmailAddress?.emailAddress)
+        )
+      );
+
+    console.log(result[0]);
+    setJsonForm(JSON.parse(result[0].jsonform));
+  };
+
   return (
-    <div>
-      Edit Form
+    <div className="p-10">
+      <h2
+        className="flex gap-2 items-center my-5 cursor-pointer hover:font-bold transition-all"
+        onClick={() => router.back()}
+      >
+        <ArrowLeft /> Back
+      </h2>
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-5">
+        <div className="p-5 border rounded-lg shadow-md">contoller</div>
+        <div className="md:col-span-2 border rounded-lg p-5 flex items-center justify-center">
+          <FormUi jsonForm={jsonForm} />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default EditForm
+export default EditForm;
