00135cff86cc67e0c2620dac5a9387f789026d2a
SidakVats <sidakvats6499@gmail.com>
2024-06-03 15:32:38 +0530

    "added payment gateway (stripe)"

---
+++ app/_data/PricingPlan.jsx
@@ -0,0 +1,14 @@
+export default[
+    {
+        link:'https://buy.stripe.com/test_7sI5kM2jpbSjbBu289',
+        price:50.00,
+        priceId:'price_1PNXKgG46HEIpSWhpCOMzYJO',
+        duration:'Monthly'
+    },
+    {
+        link:'https://buy.stripe.com/test_28o9B20bh1dF20U3ce',
+        price:400.00,
+        priceId:'price_1PNXONG46HEIpSWh3TKyslJ9',
+        duration:'Yearly'
+    }
+]
