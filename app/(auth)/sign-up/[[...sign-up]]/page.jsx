8b824e6009b2cc5d84fe3862564a95fc52546709
SidakVats <sidakvats6499@gmail.com>
2024-06-03 17:05:15 +0530

    "updated sign up form"

--- app/(auth)/sign-up/[[...sign-up]]/page.jsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.jsx
@@ -30,7 +30,7 @@ export default function Page() {
             </a>
 
             <h1 className="mt-6 text-2xl font-bold text-gray-900 sm:text-3xl md:text-4xl">
-              Welcome to Squid 🦑
+              Welcome to AI Form Generator 🦑
             </h1>
 
             <p className="mt-4 leading-relaxed text-gray-500">
