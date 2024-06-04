c23520ecda1f9f56a00eb81611b39298e9dbd58f
SidakVats <sidakvats6499@gmail.com>
2024-06-04 13:35:53 +0530

    "added steps"

--- app/_components/Hero.jsx
+++ app/_components/Hero.jsx
@@ -1,8 +1,8 @@
-import { AtomIcon, Edit, Share2 } from "lucide-react";
 import Link from "next/link";
 import React from "react";
 import { Example } from "./Example";
 import Footer from "./Footer";
+import HowItWork from "./HowItWork";
 
 function Hero() {
   return (
@@ -40,85 +40,12 @@ function Hero() {
           </div>
         </div>
       </div>
-      {/* <img src='/grid.svg' className=' absolute w-full h-[400px] '/> */}
-      <section>
-        <div className="mx-auto max-w-screen-xl px-4 pt-56 pb-28 ">
-          <div className="mx-auto max-w-2xl text-center">
-            <h2 className="text-3xl font-bold sm:text-4xl">How it Works</h2>
 
-            <p className="mt-4 text-gray-400">
-              Discover the simplicity and power of creating custom forms with
-              our AI-driven platform. Follow these three easy steps to generate,
-              customize, and share your forms effortlessly, allowing you to
-              start collecting valuable responses in no time.
-            </p>
-          </div>
+      <HowItWork />
 
-          <div className="mt-8 grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
-            <a
-              className="block rounded-xl border border-gray-800 p-8 shadow-xl transition hover:border-pink-500/10 hover:shadow-primary/15"
-              href="#"
-            >
-              <AtomIcon className="h-8 w-8" />
+      <Example />
 
-              <h2 className="mt-4 text-xl font-bold text-black">
-                Write promot for your form
-              </h2>
-
-              <p className="mt-1 text-sm text-gray-600">
-                Kickstart Your Form Creation: Simply describe your needs and let
-                our AI handle the rest.
-              </p>
-            </a>
-
-            <a
-              className="block rounded-xl border border-gray-800 p-8 shadow-xl transition hover:border-pink-500/10 hover:shadow-primary/15"
-              href="#"
-            >
-              <Edit className="h-8 w-8" />
-
-              <h2 className="mt-4 text-xl font-bold text-black">
-                Edit Your form{" "}
-              </h2>
-
-              <p className="mt-1 text-sm text-gray-600">
-                Customize with Ease: Fine-tune your form to perfection with our
-                intuitive editor.
-              </p>
-            </a>
-
-            <a
-              className="block rounded-xl border border-gray-800 p-8 shadow-xl transition hover:border-primary/10 hover:shadow-primary/15"
-              href="#"
-            >
-              <Share2 className="h-8 w-8" />
-
-              <h2 className="mt-4 text-xl font-bold text-black">
-                Share & Start Accepting Responses
-              </h2>
-
-              <p className="mt-1 text-sm text-gray-600">
-                Launch and Collect: Share your form instantly and gather
-                responses seamlessly.
-              </p>
-            </a>
-          </div>
-
-          <div className="mt-12 text-center">
-            <a
-              href="/sign-in"
-              className="inline-block rounded bg-primary px-12 py-3 text-sm font-medium text-white transition hover:bg-primary/90 focus:outline-none focus:ring focus:ring-yellow-400"
-            >
-              Get Started Today
-            </a>
-          </div>
-        </div>
-      </section>
-
-
-      <Example/>
-
-      <Footer/>
+      <Footer />
     </section>
   );
 }
---
+++ app/_components/HowItWork.jsx
@@ -0,0 +1,427 @@
+import React from "react";
+import { AtomIcon, Edit, Share2 } from "lucide-react";
+
+const HowItWork = () => {
+  return (
+    <div>
+      <section>
+        <div className="mx-auto max-w-screen-xl px-4 pt-56 pb-28 ">
+          <div className="mx-auto max-w-2xl text-center">
+            <h2 className="text-3xl font-bold sm:text-4xl">How it Works</h2>
+
+            <p className="mt-4 text-gray-400">
+              Discover the simplicity and power of creating custom forms with
+              our AI-driven platform. Follow these three easy steps to generate,
+              customize, and share your forms effortlessly, allowing you to
+              start collecting valuable responses in no time.
+            </p>
+          </div>
+
+          <div className="mt-8 grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
+            <a
+              className="block rounded-xl border border-gray-800 p-8 shadow-xl transition hover:border-pink-500/10 hover:shadow-primary/15"
+              href="#"
+            >
+              <AtomIcon className="h-8 w-8" />
+
+              <h2 className="mt-4 text-xl font-bold text-black">
+                Write promot for your form
+              </h2>
+
+              <p className="mt-1 text-sm text-gray-600">
+                Kickstart Your Form Creation: Simply describe your needs and let
+                our AI handle the rest.
+              </p>
+            </a>
+
+            <a
+              className="block rounded-xl border border-gray-800 p-8 shadow-xl transition hover:border-pink-500/10 hover:shadow-primary/15"
+              href="#"
+            >
+              <Edit className="h-8 w-8" />
+
+              <h2 className="mt-4 text-xl font-bold text-black">
+                Edit Your form{" "}
+              </h2>
+
+              <p className="mt-1 text-sm text-gray-600">
+                Customize with Ease: Fine-tune your form to perfection with our
+                intuitive editor.
+              </p>
+            </a>
+
+            <a
+              className="block rounded-xl border border-gray-800 p-8 shadow-xl transition hover:border-primary/10 hover:shadow-primary/15"
+              href="#"
+            >
+              <Share2 className="h-8 w-8" />
+
+              <h2 className="mt-4 text-xl font-bold text-black">
+                Share & Start Accepting Responses
+              </h2>
+
+              <p className="mt-1 text-sm text-gray-600">
+                Launch and Collect: Share your form instantly and gather
+                responses seamlessly.
+              </p>
+            </a>
+          </div>
+
+          {/* <div className="mt-10">
+            <h2 className=""> Detailed Steps</h2>
+            <div className="mt-5">
+              <ol className="grid grid-cols-1 divide-x divide-gray-300 overflow-hidden rounded-lg border border-gray-300 text-sm text-gray-500 sm:grid-cols-6">
+                <li className="flex items-center justify-center gap-2 p-4">
+                  <svg
+                    className="size-7 shrink-0"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M10 6H5a2 2 0 00-2 2v9a2 2 0 002 2h14a2 2 0 002-2V8a2 2 0 00-2-2h-5m-4 0V5a2 2 0 114 0v1m-4 0a2 2 0 104 0m-5 8a2 2 0 100-4 2 2 0 000 4zm0 0c1.306 0 2.417.835 2.83 2M9 14a3.001 3.001 0 00-2.83 2M15 11h3m-3 4h2"
+                    />
+                  </svg>
+
+                  <p className="leading-none">
+                    <strong className="block font-medium"> Get Started </strong>
+                    <small className="mt-1"> Click on get started and login first </small>
+                  </p>
+                </li>
+
+                <li className="relative flex items-center justify-center gap-2 bg-gray-50 p-4">
+                  <span className="absolute -left-2 top-1/2 hidden size-4 -translate-y-1/2 rotate-45 border border-gray-100 sm:block ltr:border-b-0 ltr:border-s-0 ltr:bg-white rtl:border-e-0 rtl:border-t-0 rtl:bg-gray-50"></span>
+
+                  <span className="absolute -right-2 top-1/2 hidden size-4 -translate-y-1/2 rotate-45 border border-gray-100 sm:block ltr:border-b-0 ltr:border-s-0 ltr:bg-gray-50 rtl:border-e-0 rtl:border-t-0 rtl:bg-white"></span>
+
+                  <svg
+                    className="size-7 shrink-0"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"
+                    />
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"
+                    />
+                  </svg>
+
+                  <p className="leading-none">
+                    <strong className="block font-medium"> Address </strong>
+                    <small className="mt-1"> Where we sending it? </small>
+                  </p>
+                </li>
+
+                <li className="flex items-center justify-center gap-2 p-4">
+                  <svg
+                    className="size-7 shrink-0"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M3 10h18M7 15h1m4 0h1m-7 4h12a3 3 0 003-3V8a3 3 0 00-3-3H6a3 3 0 00-3 3v8a3 3 0 003 3z"
+                    />
+                  </svg>
+
+                  <p className="leading-none">
+                    <strong className="block font-medium"> Payment </strong>
+                    <small className="mt-1"> Show us the money. </small>
+                  </p>
+                </li>
+
+
+
+
+
+                <li className="flex items-center justify-center gap-2 p-4">
+                  <svg
+                    className="size-7 shrink-0"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M3 10h18M7 15h1m4 0h1m-7 4h12a3 3 0 003-3V8a3 3 0 00-3-3H6a3 3 0 00-3 3v8a3 3 0 003 3z"
+                    />
+                  </svg>
+
+                  <p className="leading-none">
+                    <strong className="block font-medium"> Payment </strong>
+                    <small className="mt-1"> Show us the money. </small>
+                  </p>
+                </li>
+                <li className="flex items-center justify-center gap-2 p-4">
+                  <svg
+                    className="size-7 shrink-0"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M3 10h18M7 15h1m4 0h1m-7 4h12a3 3 0 003-3V8a3 3 0 00-3-3H6a3 3 0 00-3 3v8a3 3 0 003 3z"
+                    />
+                  </svg>
+
+                  <p className="leading-none">
+                    <strong className="block font-medium"> Payment </strong>
+                    <small className="mt-1"> Show us the money. </small>
+                  </p>
+                </li>
+                <li className="flex items-center justify-center gap-2 p-4">
+                  <svg
+                    className="size-7 shrink-0"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M3 10h18M7 15h1m4 0h1m-7 4h12a3 3 0 003-3V8a3 3 0 00-3-3H6a3 3 0 00-3 3v8a3 3 0 003 3z"
+                    />
+                  </svg>
+
+                  <p className="leading-none">
+                    <strong className="block font-medium"> Payment </strong>
+                    <small className="mt-1"> Show us the money. </small>
+                  </p>
+                </li>
+              </ol>
+            </div>
+          </div> */}
+
+          <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20 rounded-xl mt-10 border border-gray-800 shadow-xl transition hover:border-primary/10 hover:shadow-primary/15">
+            <div>More Detailed steps:</div>
+            <div className="grid gap-6 row-gap-10 lg:grid-cols-2">
+              <div className="lg:py-6 lg:pr-16">
+                <div className="flex">
+                  <div className="flex flex-col items-center mr-4">
+                    <div>
+                      <div className="flex items-center justify-center w-10 h-10 border rounded-full">
+                        <svg
+                          className="w-4 text-gray-600"
+                          stroke="currentColor"
+                          strokeWidth="2"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          viewBox="0 0 24 24"
+                        >
+                          <line
+                            fill="none"
+                            strokeMiterlimit="10"
+                            x1="12"
+                            y1="2"
+                            x2="12"
+                            y2="22"
+                          />
+                          <polyline
+                            fill="none"
+                            strokeMiterlimit="10"
+                            points="19,15 12,22 5,15"
+                          />
+                        </svg>
+                      </div>
+                    </div>
+                    <div className="w-px h-full bg-gray-300" />
+                  </div>
+                  <div className="pt-1 pb-8">
+                    <p className="mb-2 text-lg font-bold">Step 1</p>
+                    <p className="text-gray-700">
+                      Click on <strong>Get Started</strong> and login first.
+                    </p>
+                  </div>
+                </div>
+                <div className="flex">
+                  <div className="flex flex-col items-center mr-4">
+                    <div>
+                      <div className="flex items-center justify-center w-10 h-10 border rounded-full">
+                        <svg
+                          className="w-4 text-gray-600"
+                          stroke="currentColor"
+                          strokeWidth="2"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          viewBox="0 0 24 24"
+                        >
+                          <line
+                            fill="none"
+                            strokeMiterlimit="10"
+                            x1="12"
+                            y1="2"
+                            x2="12"
+                            y2="22"
+                          />
+                          <polyline
+                            fill="none"
+                            strokeMiterlimit="10"
+                            points="19,15 12,22 5,15"
+                          />
+                        </svg>
+                      </div>
+                    </div>
+                    <div className="w-px h-full bg-gray-300" />
+                  </div>
+                  <div className="pt-1 pb-8">
+                    <p className="mb-2 text-lg font-bold">Step 2</p>
+                    <p className="text-gray-700">
+                      After Login go to <strong>Dashboard</strong> page (on top
+                      right of navbar)
+                    </p>
+                  </div>
+                </div>
+                <div className="flex">
+                  <div className="flex flex-col items-center mr-4">
+                    <div>
+                      <div className="flex items-center justify-center w-10 h-10 border rounded-full">
+                        <svg
+                          className="w-4 text-gray-600"
+                          stroke="currentColor"
+                          strokeWidth="2"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          viewBox="0 0 24 24"
+                        >
+                          <line
+                            fill="none"
+                            strokeMiterlimit="10"
+                            x1="12"
+                            y1="2"
+                            x2="12"
+                            y2="22"
+                          />
+                          <polyline
+                            fill="none"
+                            strokeMiterlimit="10"
+                            points="19,15 12,22 5,15"
+                          />
+                        </svg>
+                      </div>
+                    </div>
+                    <div className="w-px h-full bg-gray-300" />
+                  </div>
+                  <div className="pt-1 pb-8">
+                    <p className="mb-2 text-lg font-bold">Step 3</p>
+                    <p className="text-gray-700">
+                      Click on <strong>+Create Form</strong> Button
+                    </p>
+                  </div>
+                </div>
+                <div className="flex">
+                  <div className="flex flex-col items-center mr-4">
+                    <div>
+                      <div className="flex items-center justify-center w-10 h-10 border rounded-full">
+                        <svg
+                          className="w-4 text-gray-600"
+                          stroke="currentColor"
+                          strokeWidth="2"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          viewBox="0 0 24 24"
+                        >
+                          <line
+                            fill="none"
+                            strokeMiterlimit="10"
+                            x1="12"
+                            y1="2"
+                            x2="12"
+                            y2="22"
+                          />
+                          <polyline
+                            fill="none"
+                            strokeMiterlimit="10"
+                            points="19,15 12,22 5,15"
+                          />
+                        </svg>
+                      </div>
+                    </div>
+                    <div className="w-px h-full bg-gray-300" />
+                  </div>
+                  <div className="pt-1 pb-8">
+                    <p className="mb-2 text-lg font-bold">Step 4</p>
+                    <p className="text-gray-700">
+                      Write your <strong>prompt</strong> for your form. <br />
+                      Example: <br />
+                      Create a customer feedback survey for my restaurant. Must
+                      have 10 input fields.
+                    </p>
+                  </div>
+                </div>
+                <div className="flex">
+                  <div className="flex flex-col items-center mr-4">
+                    <div>
+                      <div className="flex items-center justify-center w-10 h-10 border rounded-full">
+                        <svg
+                          className="w-6 text-gray-600"
+                          stroke="currentColor"
+                          viewBox="0 0 24 24"
+                        >
+                          <polyline
+                            fill="none"
+                            strokeWidth="2"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeMiterlimit="10"
+                            points="6,12 10,16 18,8"
+                          />
+                        </svg>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="pt-1">
+                    <p className="mb-2 text-lg font-bold">Success</p>
+                    <p className="text-gray-700" />
+                  </div>
+                </div>
+              </div>
+              <div className="relative">
+                <img
+                  className="inset-0 object-cover object-bottom w-full rounded shadow-lg h-96 lg:absolute lg:h-full"
+                  src="https://images.pexels.com/photos/3184287/pexels-photo-3184287.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
+                  alt=""
+                />
+              </div>
+            </div>
+          </div>
+
+          <div className="mt-12 text-center">
+            <a
+              href="/sign-in"
+              className="inline-block rounded bg-primary px-12 py-3 text-sm font-medium text-white transition hover:bg-primary/90 focus:outline-none focus:ring focus:ring-yellow-400"
+            >
+              Get Started Today
+            </a>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default HowItWork;
