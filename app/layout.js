e340126489f5ee299288c3beadf11ab01ea20d18
SidakVats <sidakvats6499@gmail.com>
2024-06-04 15:31:28 +0530

    "updated logo"

--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,15 @@
-
 import "./globals.css";
 import Header from "./_components/Header";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "@/components/ui/sonner";
 import ScrollTop from "./_components/ScroolTop";
 
-import { Wix_Madefor_Text  } from "next/font/google";
+import { Wix_Madefor_Text } from "next/font/google";
 
 const Wix_Madefor_Text_init = Wix_Madefor_Text({
-  subsets: ['latin'],
-  variable: '--font-wix',
+  subsets: ["latin"],
+  variable: "--font-wix",
 });
-
 
 export const metadata = {
   title: "AI Form Builder",
@@ -21,14 +19,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
-    <html lang="en">
-      <body className={Wix_Madefor_Text_init.className}>
-        <Header/>
-        <Toaster />
-        {children}
-        <ScrollTop/>
-       
+      <html lang="en">
+        <body className={Wix_Madefor_Text_init.className}>
+          <Header />
+          <Toaster />
+          {children}
+          <ScrollTop />
         </body>
-    </html></ClerkProvider>
+      </html>
+    </ClerkProvider>
   );
 }
