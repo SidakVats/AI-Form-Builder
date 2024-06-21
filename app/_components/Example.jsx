6063d3278dbac3a2ba1ce9ffc35c6971c5400030
SidakVats <sidakvats6499@gmail.com>
2024-06-21 15:59:01 +0530

    "updated 2"

--- app/_components/Example.jsx
+++ app/_components/Example.jsx
@@ -2,14 +2,14 @@ import Image from "next/image";
 
 export const Example = () => {
   return (
-    <div className="bg-white px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
-      <div className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
+    <div className="bg-white px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max_padd_container md:px-24 lg:px-8 lg:py-20">
+      <div className="max_padd_container mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
         <div>
           <p className="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider text-teal-900 uppercase rounded-full bg-teal-accent-400">
             Effortless Form Creation with AI
           </p>
         </div>
-        <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto">
+        <h2 className="max_padd_container mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto">
           <span className="relative inline-block">
             <svg
               viewBox="0 0 52 24"
@@ -43,7 +43,7 @@ export const Example = () => {
           purpose.
         </p>
       </div>
-      <div className="grid gap-8 row-gap-5 md:row-gap-8 lg:grid-cols-3">
+      <div className="grid gap-8 row-gap-5 md:row-gap-8 lg:grid-cols-3 max_padd_container">
         <div className="p-5 duration-300 transform bg-white border-2 border-dashed rounded shadow-sm border-deep-purple-accent-100 hover:-translate-y-2">
           <div className="flex items-center mb-2 gap-3">
             <Image src="./one.svg" alt="one svg" width={25} height={10}/>
